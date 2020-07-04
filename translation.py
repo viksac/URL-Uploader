bdabcc56bcff5105865e5b0f82b1830b9f73c78a
PrG <65294906+prgofficial@users.noreply.github.com>
2020-07-04 14:56:17 +0200

    Update translation.py

--- translation.py
+++ translation.py
@@ -51,8 +51,8 @@ Expires on: 31/12/2020"""
 --------
 Send /me to know current plan details
 
-Support Group : @InFoTelGroup
-© @TGBotsZ & @CWProjects"""
+Support Group : @MoviEzoNlysuppOrT
+© @MovieZonlYheRe"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
